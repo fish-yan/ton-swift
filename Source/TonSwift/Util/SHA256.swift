1aecebbea6f5d7cfb8ee8f3e85e1c41463ac9754
Yan <15316488+fish-yan@users.noreply.github.com>
2024-08-28 18:00:26 +0800

    Update SHA256.swift
    internal sha256

--- Source/TonSwift/Util/SHA256.swift
+++ Source/TonSwift/Util/SHA256.swift
@@ -2,7 +2,7 @@ import CommonCrypto
 import Foundation
 
 extension Data {
-    public func sha256() -> Self {
+    func sha256() -> Self {
         var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
         withUnsafeBytes {
             _ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
